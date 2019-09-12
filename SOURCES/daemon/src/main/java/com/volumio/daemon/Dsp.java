c6319a4e7ab9f142c2eadbe0ac706f1ef65e8e74
Bjorn Hijmans <bjorn.hijmans@jobdigger.nl>
2019-09-12 20:39:44 +0200

    WIP

--- SOURCES/daemon/src/main/java/com/volumio/daemon/Dsp.java
+++ SOURCES/daemon/src/main/java/com/volumio/daemon/Dsp.java
@@ -15,7 +15,7 @@ public class Dsp {
     @Autowired
     private Environment env;
 
-    @Value("${volumiodaemon.initial_volume:10}")
+    @Value("${volumiodaemon.initial_volume:-120}")
     private int volume;
 
     @Value("${volumiodaemon.initial_input}")
@@ -29,8 +29,12 @@ public class Dsp {
 
         this.volume = volume;
 
+        double dsp_volume = Math.pow(10.0, volume/20.0);
+
+        logger.info(Double.toString(dsp_volume));
+
         // 4fac00012cd7
-        String data = this.makeParameter(20396, volume);
+        String data = this.makeParameter(20396, dsp_volume);
 
         logger.info(data);
     }
@@ -43,36 +47,53 @@ public class Dsp {
         this.input = input;
     }
 
-    String makeParameter(Integer reg, float val)
+    String makeParameter(Integer reg, double val)
     {
         String data = "";
 
         data = data + Integer.toHexString((reg >> 8) & 0x000000FF);
         data = data + Integer.toHexString(reg & 0x000000FF);
 
-        Integer val_824 = this.convertTo824(val);
+        long val_824 = this.convertTo824(val);
 
-        data = data + Integer.toHexString((val_824 >> 24) & 0x000000FF);
-        data = data + Integer.toHexString((val_824 >> 16) & 0x000000FF);
-        data = data + Integer.toHexString((val_824 >> 8) & 0x000000FF);
-        data = data + Integer.toHexString(val_824 & 0x000000FF);
+        data = data + String.format("%02X", (byte)((val_824 >> 24) & 0x000000FF));
+
+        logger.info(data);
+
+        data = data + String.format("%02X", (byte)((val_824 >> 16) & 0x000000FF));
+
+        logger.info(data);
+
+        data = data + String.format("%02X", (byte)((val_824 >> 8) & 0x000000FF));
+
+        logger.info(data);
+
+        data = data + String.format("%02X", (byte)(val_824 & 0x000000FF));
+
+        logger.info(data);
 
         return data;
     }
 
     // https://wiki.analog.com/resources/tools-software/sigmastudio/usingsigmastudio/numericformats
-    Integer convertTo824( float val )
+    long convertTo824( double val )
     {
-        float fractpart;
-        int intpart;
+        double fractpart;
+        short intpart;
 
-        int ret;
+        long ret;
 
-        intpart = (int) Math.floor( val );
+        intpart = (short) Math.floor( val );
         fractpart = val - intpart;
 
+        logger.info(Long.toBinaryString(intpart));
+
+        logger.info(Float.toString(intpart)); logger.info(Double.toString(fractpart));
+
         ret = ((intpart << 24) & 0xff000000)
-                + ((int)(fractpart * 16777216.f) & 0x00ffffff);
+                + ((short)(fractpart * 16777216.f) & 0x00ffffff);
+
+        logger.info(Long.toBinaryString(ret));
 
         return ret;
     }
