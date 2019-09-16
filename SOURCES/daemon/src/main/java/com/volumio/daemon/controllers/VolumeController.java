76f8575a6f279b07806ac16780d4d76e29d94d02
Bjorn Hijmans <bjorn.hijmans@jobdigger.nl>
2019-09-16 08:28:09 +0200

    WIP

--- SOURCES/daemon/src/main/java/com/volumio/daemon/controllers/VolumeController.java
+++ SOURCES/daemon/src/main/java/com/volumio/daemon/controllers/VolumeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 
 @Controller
 @Validated
