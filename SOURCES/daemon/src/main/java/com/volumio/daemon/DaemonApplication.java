1e093cf224a4e3c98637c985e6068ac0fe6002b3
Bjorn Hijmans <bjorn.hijmans@jobdigger.nl>
2019-12-08 22:00:58 +0100

    WIP

--- SOURCES/daemon/src/main/java/com/volumio/daemon/DaemonApplication.java
+++ SOURCES/daemon/src/main/java/com/volumio/daemon/DaemonApplication.java
@@ -1,15 +1,32 @@
 package com.volumio.daemon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 
 @SpringBootApplication()
 public class DaemonApplication {
 
+    @Autowired
+    Volume volume;
+
     public static void main(String[] args) {
 
         SpringApplication.run(DaemonApplication.class, args);
+    }
+
+    Logger logger = LoggerFactory.getLogger(Volumio.class);
+
+
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        logger.info("Initialized");
+        volume.setVolume(10);
     }
 
 }
