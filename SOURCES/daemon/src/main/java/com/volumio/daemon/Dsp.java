14d29f5c6cf3159e389200f533d7a1148dc17352
Bjorn Hijmans <bjorn.hijmans@jobdigger.nl>
2019-12-10 21:33:43 +0100

    WIP

--- SOURCES/daemon/src/main/java/com/volumio/daemon/Dsp.java
+++ SOURCES/daemon/src/main/java/com/volumio/daemon/Dsp.java
@@ -49,6 +49,11 @@ public class Dsp extends AbstractSpringSerialPortConnector {
 
         logger.info("Calculated: " + data);
 
+        this.sendData("D" + data);
+    }
+
+    void sendData(String data)
+    {
         try {
             this.sendMessage("D" + data);
             this.sendMessage("\n");
@@ -64,7 +69,99 @@ public class Dsp extends AbstractSpringSerialPortConnector {
     }
 
     void setInput(String input) {
+
         this.input = input;
+        int oldvol = this.volume;
+        this.setVolume(0);
+
+        // delay
+        for(int i = 0; i < 4; i++)
+            this.sendData("D" + "000000000000");
+
+        switch(input)
+        {
+            case "usb":
+                // channel 1
+                this.sendData("D" + "4fde00000000");
+                this.sendData("D" + "4fe900000001");
+
+                // channel 2
+                this.sendData("D" + "4fda00000001");
+                this.sendData("D" + "4fe800000001");
+
+                break;
+            case "analog_1":
+
+                // channel 1
+                this.sendData("D" + "4fdd00000000");
+                this.sendData("D" + "4fe900000000");
+
+                // channel 2
+                this.sendData("D" + "4fdb00000001");
+                this.sendData("D" + "4fe800000000");
+
+                break;
+            case "analog_2":
+
+                // channel 1
+                this.sendData("D" + "4fdd00000002");
+                this.sendData("D" + "4fe900000000");
+
+                // channel 2
+                this.sendData("D" + "4fdb00000003");
+                this.sendData("D" + "4fe800000000");
+
+                break;
+            case "analog_3":
+
+                // channel 1
+                this.sendData("D" + "4fdd00000004");
+                this.sendData("D" + "4fe900000000");
+
+                // channel 2
+                this.sendData("D" + "4fdb00000005");
+                this.sendData("D" + "4fe800000000");
+
+                break;
+            case "analog_4":
+
+                // channel 1
+                this.sendData("D" + "4fdd00000006");
+                this.sendData("D" + "4fe900000000");
+
+                // channel 2
+                this.sendData("D" + "4fdb00000007");
+                this.sendData("D" + "4fe800000000");
+
+                break;
+            case "optical_1":
+
+                // channel 1
+                this.sendData("D" + "4fe100000000");
+                this.sendData("D" + "4fe900000004");
+
+                // channel 2
+                this.sendData("D" + "4fd900000001");
+                this.sendData("D" + "4fe800000004");
+
+                break;
+            case "optical_2":
+
+                break;
+            case "optical_3":
+
+                break;
+            case "optical_4":
+
+                break;
+
+        }
+
+        // delay
+        for(int i = 0; i < 4; i++)
+            this.sendData("D" + "000000000000");
+
+        this.setVolume(oldvol);
     }
 
     String makeParameter(Integer reg, double val)
